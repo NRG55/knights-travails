dab1a52ec373a9c620207f51613f14f783c0678f
NRG55 <maloj_1980@yahoo.fr>
2025-02-11 18:59:17 +0000

    Update(index.js): added print result

--- Graph.js
+++ Graph.js
@@ -1,8 +1,8 @@
 import Knight from "./Knight.js";
 
 export default class Graph {
-    constructor(size = 8 ) {
-        this.vertices = [];
+    constructor(size = 8 ) { // Size 8 * 8
+        this.vertices = [];  // initBoard() fills up the array with vertices 
         this.board = this.initBoard(size);
         this.adjacentList = new Map();
       
@@ -16,12 +16,12 @@ export default class Graph {
         for (let i = 0; i < size; i++) {
             board[i] = [];
     
-            for (let j = 0; j < size; j++) {
-                x = j + 1;
-                y = 8 - i;
+            for (let j = 0; j < size; j++) {                                              
+                x = j + 1;                    
+                y = 8 - i;                    
+                                              // inserts x and y coordinates for each square on the board
+                board[i][j] = `[${x}, ${y}]`; // just to display the array in the console similar to X Y graph:)
 
-                board[i][j] = `[${x}, ${y}]`; // inserts x and y coordinates for each square on the board
-                
                 this.vertices.push(`[${x}, ${y}]`); // pushed as a string --> will be used as a key in the adjacentList
             };
         };
--- index.js
+++ index.js
@@ -2,7 +2,19 @@ import Knight from "./Knight.js";
 
 const knight = new Knight();
 
-console.log(knight.moves([1, 1], [3, 8]))
+const moves = knight.moves([1, 1], [3, 8]);
+
+console.log(`=> You made it in ${moves.length - 1} moves! Here's your path from ${moves[0]} to ${moves[moves.length - 1]}:`);
+
+let number = 1;
+
+moves.forEach((move, index) => {
+    if (index === 0) {
+        return;
+    };   
+    console.log(`Move ${number}: ${move}`);
+    number++
+});
 
 
 
